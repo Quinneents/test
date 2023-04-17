04d42c59f78e0a793ac50839f87d6c540f9c26e8
Quinneents <130966311+Quinneents@users.noreply.github.com>
2023-04-17 11:40:57 +0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,3 @@
+function seealllatest() {
+         document.getElementsByClassName("latest_movies").style ="width:100%;height:auto;left:0px; position:fixed;z-index:9;top:55px;display:flex; overflow-x:none;overflow-y:auto; whitespace:pre-wrap;";
+      }
